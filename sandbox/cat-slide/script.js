ad36cbb556d1f1c230b43e73fe5a26a850be7cc1
alannarisse <alannarisse@users.noreply.github.com>
2026-01-02 12:54:00 -0800

    added a new homepage with links to fun things

---
+++ sandbox/cat-slide/script.js
@@ -0,0 +1,18 @@
+$('.listen').click(function(){
+   $('.player').addClass('slide')
+  })
+$('.player').click(function(){
+  $('.player').removeClass('slide')
+})
+$('.why').click(function(){
+   $('.own').addClass('slide')
+  })
+$('.own').click(function(){
+  $('.own').removeClass('slide')
+})
+$('.products').click(function(){
+  $('.shop').addClass('slide')
+})
+$('.shop').click(function(){
+  $('.shop').removeClass('slide')
+})
